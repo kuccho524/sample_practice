f5518cc86281d8a7d7376378001739421e160934
kuccho524 <kucchachachanocha@gmail.com>
2021-04-14 04:35:52 +0000

    Test

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '20798545873a03f21406e46a8babb12fbd798d523f7a527132180e84a7739857e2ee459b65a1a85d59b7c40aba9ed2240e147c18ad0c767fa97967f3b9ef14e0'
